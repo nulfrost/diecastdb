2b4d725867dae6adf4b992b7ad912bdc9cfc9031
nulfrost <khadane.miller@gmail.com>
2025-06-10 11:53:14 -0400

    create index route

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,7 +3,17 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text(`
+  < Hotwheels API >
+
+  Version: 0.1
+
+  API Routes: /hotwheels, /hotwheels/{id}
+
+  Code: https://github.com/nulfrost/hotwheels-api
+
+  Last Updated: ${new Date().toLocaleString()}
+  `)
 });
 
 export default app;
