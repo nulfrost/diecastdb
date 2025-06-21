0d031ef9c4c5fdc50b4588639f11bb5ecfdc7524
nulfrost <khadane.miller@gmail.com>
2025-06-21 17:09:21 -0400

    add designers endpoints

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -11,7 +11,7 @@ app.get("/", (c) => {
   return c.text(`
   < Hotwheels API >
 
-  API Routes: /hotwheels, /hotwheels/{id}
+  API Routes: /hotwheels, /hotwheels/{id}, /designers, /designers/{id}
 
   Code: https://github.com/nulfrost/hotwheels-api
 
@@ -34,10 +34,21 @@ app.get("/hotwheels/:id", async (c) => {
   return c.json({ data: result })
 })
 
-// designers
+app.get("/designers", async (c) => {
+  const db = drizzle(c.env.DB, { schema });
+  const result = await db.query.designers.findMany()
+  return c.json({ data: result })
+})
 
 
-// desingers by id
+app.get("/designers/:id", async (c) => {
+  const { id } = c.req.param()
+  const db = drizzle(c.env.DB, { schema });
+  const result = await db.query.designers.findFirst({
+    where: clause.eq(schema.designers.id, +id)
+  })
+  return c.json({ data: result })
+})
 
 app.get("/healthcheck", (c) => {
   return c.json({ message: "OK", status: 200 });
